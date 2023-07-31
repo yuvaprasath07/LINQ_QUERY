a0a0fc7bff4415fe3d44e0af14ee64662ea4012f
yuvaprasath07 <yuvaparasath.visualapp@gmail.com>
2023-07-31 22:05:21 +0530

    linq commit

---
+++ Controllers/StudenInfoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentInfo;
+
+namespace JSON_LINQ.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudenInfoController : ControllerBase
+    {
+        private readonly IStudentLogicLayer studentLogicLayer;
+
+        public StudenInfoController(IStudentLogicLayer _studentLogicLayer)
+        {
+            studentLogicLayer= _studentLogicLayer;
+        }
+
+        [HttpGet("StudentDetails")]
+        public IActionResult getStudent()
+        {
+            var data = studentLogicLayer.getAll();
+            return Ok(data);
+        }
+
+        [HttpGet("FoodProduct")]
+        public IActionResult GetProduct()
+        {
+            var data=studentLogicLayer.GetProduct();
+            return Ok(data);
+        }
+
+    }
+}
